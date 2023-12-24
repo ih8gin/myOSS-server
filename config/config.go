a9d9dfe0a9604d1366ba77d031185585062f8f64
ih8gin <erictxl@outlook.com>
2023-12-24 01:06:04 +0000

    to linux

--- config/config.go
+++ config/config.go
@@ -2,6 +2,6 @@ package config
 
 const RABBITMQ_SERVER = "amqp://myoss:myoss@14.29.248.20:5672"
 const ES_SERVER = "14.29.248.20:9200"
-const STORAGE_ROOT = "C:/workplace/projects/MyOSS/Server/testData/1"
+const STORAGE_ROOT = "/home/myoss/data"
 const DATANODE_LISTEN_ADDRESS = "127.0.0.1:55551"
 const APINODE_LISTEN_ADDRESS = "127.0.0.1:55550"
--- dataServer/temp/TempCleanWorker.go
+++ dataServer/temp/TempCleanWorker.go
@@ -4,7 +4,6 @@ import (
 	"MyOSS/config"
 	"os"
 	"path/filepath"
-	"syscall"
 	"time"
 )
 
@@ -13,8 +12,13 @@ func TempCleanWorcker() {
 		files, _ := filepath.Glob(config.STORAGE_ROOT + "/temp/*")
 		for i := range files {
 			file_info, _ := os.Stat(files[i])
-			winFileAttr := file_info.Sys().(*syscall.Win32FileAttributeData)
-			if time.Unix(winFileAttr.LastWriteTime.Nanoseconds()/1e9, 0).Add(10 * time.Minute).Before(time.Now()) {
+			// windows operator
+			//winFileAttr := file_info.Sys().(*syscall.Win32FileAttributeData)
+			//if time.Unix(winFileAttr.LastWriteTime.Nanoseconds()/1e9, 0).Add(10 * time.Minute).Before(time.Now()) {
+			//	os.Remove(files[i])
+			//}
+			// linux operator
+			if file_info.ModTime().Add(10 * time.Minute).Before(time.Now()) {
 				os.Remove(files[i])
 			}
 		}
